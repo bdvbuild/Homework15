629d4393df05693146ed62f7322d530709f0356c
Dmitriy Baranov <bdvbuild@gmail.com>
2022-11-21 15:08:30 +0400

    Добавьте файлы проекта.

---
+++ Homework15/ISeries.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework15
+{
+    interface ISeries
+    {
+        void SetStart(int x);
+        int GetNext();
+        void Reset();
+    }
+    class ArithProgression : ISeries
+    {
+        int startValue;
+        int currentValue;
+        int step;
+        public int GetNext()
+        {
+            currentValue += step;
+            return currentValue;
+        }
+
+        public void Reset()
+        {
+            currentValue = startValue;
+        }
+
+        public void SetStart(int x)
+        {
+            startValue = x;
+            currentValue = startValue;
+        }
+        public void SetStep(int s)
+        {
+            step = s;
+        }
+    }
+    class GeomProgression : ISeries
+    {
+        int startValue;
+        int currentValue;
+        int step;
+
+        public int GetNext()
+        {
+            currentValue *= step;
+            return currentValue;
+        }
+
+        public void Reset()
+        {
+            currentValue = startValue;
+        }
+
+        public void SetStart(int x)
+        {
+            startValue = x;
+            currentValue = startValue;
+        }
+        public void SetStep(int s)
+        {
+            step = s;
+        }
+
+    }
+}
---
+++ Homework15/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Разработать интерфейс ISeries генерации ряда чисел. Интерфейс содержит следующие элементы:
+
+//метод void SetStart(int x) -устанавливает начальное значение
+//метод int GetNext() -возвращает следующее число ряда
+//метод void Reset() -выполняет сброс к начальному значению
+
+//Разработать классы ArithProgression и GeomProgression, которые реализуют интерфейс ISeries.
+//В классах реализовать методы интерфейса в соответствии с логикой арифметической и геометрической прогрессии соответственно.
+
+namespace Homework15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithProgression progression1 = new ArithProgression();
+            Console.WriteLine("--------------------------\nАрифметическая прогрессия");
+            progression1.SetStart(10);
+            progression1.SetStep(2);
+            Console.WriteLine(progression1.GetNext());
+            Console.WriteLine(progression1.GetNext());
+            Console.WriteLine(progression1.GetNext());
+            progression1.Reset();
+            Console.WriteLine(progression1.GetNext());
+            Console.WriteLine(progression1.GetNext());
+
+
+            GeomProgression progression2 = new GeomProgression();
+            Console.Write("--------------------------\nГеометрическая прогрессия\nВведите стартовое число:");
+            progression2.SetStart(Convert.ToInt32(Console.ReadLine()));
+            Console.Write("Введите шаг прогрессии: ");
+            progression2.SetStep(Convert.ToInt32(Console.ReadLine()));
+            Console.Write("Необходимое количество чисел: ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            while (i!=0) 
+            {
+                Console.Write($"{progression2.GetNext()} ");
+                i -= 1;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
